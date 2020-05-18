07e5df29f9c219af573fd013ace06628b208693c
Aznbai <rlikholat@gmail.com>
2020-05-18 02:46:01 +0200

    Create firebase_blink_test_1.ino

---
+++ firebase_blink_test_1/firebase_blink_test_1.ino
@@ -0,0 +1,86 @@
+#include <WiFi.h>
+#include "FirebaseESP32.h"
+
+
+#define FIREBASE_HOST "xxxxt.firebaseio.com" //Change to your Firebase RTDB project ID e.g. Your_Project_ID.firebaseio.com
+#define FIREBASE_AUTH "xxxx" //Change to your Firebase RTDB secret password
+#define WIFI_SSID "xxxx"
+#define WIFI_PASSWORD "xxxx"
+
+
+//Define Firebase Data objects
+FirebaseData firebaseData1;
+
+bool swState = false;
+void streamCallback(StreamData data)
+{
+
+  if (data.dataType() == "boolean") {
+    if (data.boolData())
+      Serial.println("Set  to High");
+    else
+      Serial.println("Set to Low");
+    digitalWrite(LED_BUILTIN, data.boolData());
+  }
+
+
+}
+
+
+void streamTimeoutCallback(bool timeout)
+{
+  if (timeout)
+  {
+    Serial.println();
+    Serial.println("Stream timeout, resume streaming...");
+    Serial.println();
+  }
+}
+
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println();
+  Serial.println();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  if (!Firebase.beginStream(firebaseData1))
+  {
+    Serial.println("Could not begin stream");
+    Serial.println("REASON: " + firebaseData1.errorReason());
+    Serial.println();
+  }
+  Firebase.setStreamCallback(firebaseData1, streamCallback, streamTimeoutCallback);
+}
+
+void loop()
+{
+  // get start state
+  if (Firebase.getBool(firebaseData1, "/start")) {
+    Serial.println("start state: "+firebaseData1.boolData());
+  } else {
+    Serial.println("error geting start state:" + firebaseData1.errorReason());
+  }
+    // get stack0 state
+  if (Firebase.getBool(firebaseData1, "/start")) {
+    Serial.println("start state: "+firebaseData1.boolData());
+  } else {
+    Serial.println("error geting start state:" + firebaseData1.errorReason());
+  }
+  
+}
